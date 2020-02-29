951f929ea44c519e77fac8ed9b0c1a68e9a2b5c3
Usuario de Logon Laboratorio Petit Thouars 385 <LAB-USR-PT385-1406@grupoutp.net>
2020-02-29 13:36:03 -0500

    Contador

--- src/App.js
+++ src/App.js
@@ -1,34 +1,48 @@
-import React, { useState } from 'react';
-import MyComponent from './MyComponet'
+//const React = require('react')
+import React, { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const [ count, setCount ] = useState(0)
-
-  const addMount = () => {
-    setCount(count+1)
-  } 
-
-  const removeMount = () => {
-    setCount(count-1)
-  } 
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <MyComponent name="Johan"/>
-        <button onClick={addMount}>
-          +
-        </button>
-        <button onClick={removeMount}>
-          -
-        </button>
-        <h1>{count}</h1>
-      </header>
-    </div>
-  );
+//Componentes = clase
+/*class App(){
+    constructor(){
+        this.state:{
+            name: 'ejemplo'
+        }
+    }
+    render(){
+        <div>
+            Hola mundo
+        </div>
+    }
 }
+*/
+
+const App = () => {
+    //const [name, setName] = useState('Renato')
+    const [count, setCount] = useState(0)
+   const sumar = () => {
+       setCount(count+1)
+   }
+   const restar = () => {
+       setCount(count-1)
+   }
+    return (
+        <div className="App">
+            <div className="App-header">
+                <img src={logo}
+                className="App-logo"></img>
+                <div>{count}</div>
+                <button onClick={sumar}>
+                    SUMAR
+                </button>
+                <button onClick={restar}>
+                    RESTAR
+                </button>
+            </div>
+        </div>
+    )
+}
+
 
 export default App;
